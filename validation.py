477e00f7e3cbac2607b6f6ec1a7832f3d238c38f
davidlainesv <david.dev@outlook.com>
2023-03-01 02:47:48 -0600

    ok

--- validation.py
+++ validation.py
@@ -95,7 +95,7 @@ def run_experiment(config=None, log_to_wandb=True, verbose=0):
 
     # train for a few more epochs
     if base_model is not None:
-        extra_epochs = 10
+        extra_epochs = 20
         base_model.trainable = True
         metrics = [tf.keras.metrics.TopKCategoricalAccuracy(
             k=1, name='top_1', dtype=tf.float32)]
