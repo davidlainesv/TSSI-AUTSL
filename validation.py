9d51ecad8a835cc9286fa562ac69b673fa89fff2
davidlainesv <david.dev@outlook.com>
2023-03-01 02:30:46 -0600

    ok

--- validation.py
+++ validation.py
@@ -97,13 +97,19 @@ def run_experiment(config=None, log_to_wandb=True, verbose=0):
     if base_model is not None:
         extra_epochs = 10
         base_model.trainable = True
-        metrics = [tf.keras.metrics.TopKCategoricalAccuracy(
-            k=1, name='top_1', dtype=tf.float32)]
-        model.compile(
-            optimizer=tf.keras.optimizers.Adam(1e-5),  # Low learning rate
-            loss='categorical_crossentropy',
-            metrics=metrics,
-        )
+        # metrics = [tf.keras.metrics.TopKCategoricalAccuracy(
+        #     k=1, name='top_1', dtype=tf.float32)]
+        # model.compile(
+        #     optimizer=tf.keras.optimizers.Adam(1e-5),  # Low learning rate
+        #     loss='categorical_crossentropy',
+        #     metrics=metrics,
+        # )
+        optimizer = build_sgd_optimizer(initial_learning_rate=config['initial_learning_rate'],
+                                        maximal_learning_rate=config['maximal_learning_rate'],
+                                        momentum=config['momentum'],
+                                        nesterov=config['nesterov'],
+                                        step_size=config['step_size'],
+                                        weight_decay=config['weight_decay'])
         model.fit(train_dataset,
                   epochs=extra_epochs,
                   verbose=verbose,
