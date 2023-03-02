78fc1f69a54cb177d315ff7b4bf9d36cc32ef914
davidlainesv <david.dev@outlook.com>
2023-03-01 23:12:06 -0600

    ok

--- preprocessing.py
+++ preprocessing.py
@@ -29,7 +29,7 @@ class PadIfLessThan(tf.keras.layers.Layer):
         shape = tf.shape(images)
         shape = tf.constant([shape[0],
                              self.frames,
-                             shape[2],
+                             135,
                              shape[3]])
         images = tf.tile(images, shape)
         return images
