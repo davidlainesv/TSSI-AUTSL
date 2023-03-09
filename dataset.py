676be02de46cf1a2eb015de1ece162d53e3321b1
davidlainesv <david.dev@outlook.com>
2023-03-09 03:32:50 -0600

    ok

--- dataset.py
+++ dataset.py
@@ -15,7 +15,7 @@ class LayerType(IntEnum):
 LayerDict = {
     'random_speed': {
         'type': LayerType.Augmentation,
-        'layer': RandomSpeed(min_frames=60, max_frames=MIN_INPUT_HEIGHT, seed=5),
+        'layer': RandomSpeed(min_frames=40, max_frames=128, seed=5),
     },
     'random_rotation': {
         'type': LayerType.Augmentation,
